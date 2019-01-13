048f971b1a3cd839e7652727604399b771b868a2
Marc Vilchez <ht.m4rck@gmail.com>
2019-01-12 21:25:02 -0500

    TEST GIT

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 #UTIL
+
 #COMPONENTES
 require_once "component/modal.php";
 require_once "component/div.php";
@@ -10,12 +11,16 @@ require_once "component/input.php";
 require_once "component/navitem.php";
 require_once "component/option.php";
 require_once "component/select.php";
+
 #Conexion y enrutador
 require_once "model/conexion.php";
 require_once "model/page.php";
+
 #CONTROL
 require_once "control/controller.php";
+
 #modelos 
+
 #beans
 
 $controller = new Controller;
